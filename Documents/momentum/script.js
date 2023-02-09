4fca1b49c69797bb173f89d43e79526a61d1469b
최유림 <choiyurim@choeyulim-ui-MacBookAir.local>
2023-02-09 23:54:27 +0900

    markdown

---
+++ Documents/momentum/script.js
@@ -0,0 +1,14 @@
+const player = {
+    name: "ulmu",
+    points: 10,
+    pretty: true,
+    fat: "little bit",
+};
+
+console. log(player);
+player.lastName = "choi";
+player.points = player.points + 16;
+console. log(player);
+
+
+
